25ed5e6f1ff4e5adadcd12419ef2a0bea6a1f3a0
Filipe Ximenes <filipeximenes@gmail.com>
2015-05-13 11:35:52 -0300

    updating tapioca wrapper version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except (IOError, ImportError):
 
 package = 'tapioca_facebook'
 requirements = [
-    'tapioca-wrapper==0.2.1',
+    'tapioca-wrapper==0.2.4',
     'requests-oauthlib==0.4.2',
 ]
 test_requirements = [
