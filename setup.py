fd59179fa758b1ac423dc51e7425c9140aec6144
Filipe Ximenes <filipeximenes@gmail.com>
2015-09-17 12:13:55 -0300

    tying wrapper to any version under 0.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except (IOError, ImportError):
 
 package = 'tapioca_facebook'
 requirements = [
-    'tapioca-wrapper==0.5.0',
+    'tapioca-wrapper<=0.6',
     'requests-oauthlib==0.4.2',
 ]
 test_requirements = [
